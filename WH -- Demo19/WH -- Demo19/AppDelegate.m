2ec0d86a1348ee7333fae914dfc6c5efdd8b8d94
QIUGUI <15201314165@163.com>
2017-05-03 00:44:25 +0800

    test

--- WH -- Demo19/WH -- Demo19/AppDelegate.m
+++ WH -- Demo19/WH -- Demo19/AppDelegate.m
@@ -16,7 +16,12 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    
+    
+    
+    NSLog(@"test -- 123");
+    
     return YES;
 }
 
